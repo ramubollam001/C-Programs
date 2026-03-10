a4a6c35bc2491bc4b542778ec3ba33d36fe3bc02
Bollam Ramu <ramubollam001@gmail.com>
2026-03-10 23:26:02 +0530

    Create singlelinkedlist.cpp

---
+++ singlelinkedlist.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class node{
+ public:
+int data;
+node* next;
+void insertBeginning(node* &head,int value)
+{
+    node* newnode=new node;
+    newnode->data=value;
+    newnode->next=head;
+    head=newnode;
+}
+void print(node* head)
+{
+cout<<"linked list:";
+while(head!=nullptr)
+{
+  cout<<head->data<<" "; 
+  head=head->next;
+}
+cout<<"null";
+}
+};
+int main() {
+   node n;
+   node* head=nullptr;
+   n.insertBeginning(head,10);
+   n.insertBeginning(head,20);
+   n.print(head);
+    return 0;
+}
+//output:
+//linked list:20 10 null
